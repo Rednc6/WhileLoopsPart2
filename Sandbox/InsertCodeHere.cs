695983c0c568209ffbeed33660255b13bb9dc18a
Michael Haurholm Sternbo <mich475r@edu.easj.dk>
2016-10-13 12:40:21 +0200

    while loop that parses userintpu into int and checks if value between 1-10

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -10,16 +10,23 @@ namespace Sandbox
 
             CubeCalculator cc = new CubeCalculator();
 
-            String userInput = cc.GetUserInput("Please enter an integer number:");
-            if (cc.IsANumber(userInput))
+            
+            String userInput = cc.GetUserInput("Please enter an integer number between 1 and 10:");
+
+            while(Int32.Parse(userInput) <= 10 && Int32.Parse(userInput) >= 1)
             {
-                int value = cc.ConvertToNumber(userInput);
-                int result = cc.CalculateCube(value);
-                Console.WriteLine("The cube of {0} is {1}", value, result);
-            }
-            else
-            {
-                Console.WriteLine("Could not calculate...");
+                if (cc.IsANumber(userInput))
+                {
+                    int value = cc.ConvertToNumber(userInput);
+                    int result = cc.CalculateCube(value);
+                    Console.WriteLine("The cube of {0} is {1}", value, result);
+                    userInput = cc.GetUserInput("Please enter an integer number between 1 and 10:");
+
+                }
+                else
+                {
+                    Console.WriteLine("Could not calculate...");
+                }
             }
 
             // The LAST line of code should be ABOVE this line
